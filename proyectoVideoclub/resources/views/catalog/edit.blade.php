07c3a715f2e409cbc7e97384e7237d7fc8aafe15
jp12emilio <jp12emilio@gmail.com>
2019-01-22 11:08:40 +0100

    Ejercicio 3 Laravel 5

--- proyectoVideoclub/resources/views/catalog/edit.blade.php
+++ proyectoVideoclub/resources/views/catalog/edit.blade.php
@@ -17,27 +17,27 @@
 
             <div class="form-group">
                <label for="title">Título</label>
-               <input type="text" name="title" id="title" class="form-control" value="{{$pelicula['title']}}">
+               <input type="text" name="title" id="title" class="form-control" value="{{$pelicula->title}}">
             </div>
 
             <div class="form-group">
                <label for="year">Año</label>
-               <input type="text" name="year" id="year" class="form-control" value="{{$pelicula['year']}}">
+               <input type="text" name="year" id="year" class="form-control" value="{{$pelicula->year}}">
             </div>
 
             <div class="form-group">
                <label for="director">Director</label>
-               <input type="text" name="director" id="director" class="form-control" value="{{$pelicula['director']}}">
+               <input type="text" name="director" id="director" class="form-control" value="{{$pelicula->director}}">
             </div>
 
             <div class="form-group">
                <label for="poster">Poster</label>
-               <input type="text" name="poster" id="poster" class="form-control" value="{{$pelicula['poster']}}">
+               <input type="text" name="poster" id="poster" class="form-control" value="{{$pelicula->poster}}">
             </div>
 
             <div class="form-group">
                <label for="synopsis">Resumen</label>
-               <textarea name="synopsis" id="synopsis" class="form-control" rows="3">{{$pelicula['synopsis']}}</textarea>
+               <textarea name="synopsis" id="synopsis" class="form-control" rows="3">{{$pelicula->synopsis}}</textarea>
             </div>
 
             <div class="form-group text-center">
--- proyectoVideoclub/resources/views/catalog/show.blade.php
+++ proyectoVideoclub/resources/views/catalog/show.blade.php
@@ -4,30 +4,30 @@
 <div class="row">
 
 	<div class="col-sm-4">
-			<img src="{{$pelicula['poster']}}" style="height:300px"/>
+			<img src="{{$pelicula->poster}}" style="height:300px"/>
 	</div>
 	<div class="col-sm-8">
 		<h1>
-			{{$pelicula['title']}}
+			{{$pelicula->title}}
 		</h1>
 		<h5>
-			Año: {{$pelicula['year']}}
+			Año: {{$pelicula->year}}
 		</h5>
 		<h5>
-			Director: {{$pelicula['director']}}
+			Director: {{$pelicula->director}}
 		</h5>
 		<br>
 		<p>
-			<strong>Resumen: </strong>{{$pelicula['synopsis']}}
+			<strong>Resumen: </strong>{{$pelicula->synopsis}}
 		</p>
-		@if($pelicula['rented'] == false)
+		@if($pelicula->rented == false)
 			<strong>Estado: </strong> Película disponible.<br><br>
 			<a class="btn btn-primary" type="button">Alquilar película</a>
 		@else
 			<strong>Estado: </strong> Película actualmente alquilada.<br><br>
 			<a class="btn btn-danger" type="button"">Devolver película</a>
 		@endif
-		<a class="btn btn-warning" type="button" href="{{url('/catalog/edit/' . $id)}}">Editar película</a>
+		<a class="btn btn-warning" type="button" href="{{url('/catalog/edit/' . $pelicula->id)}}">Editar película</a>
 		<a class="btn btn-default" type="button" href="{{url('/catalog/')}}">Volver al listado</a>
 
 	</div>
